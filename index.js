13a64e94ede5d3d3513703b4ff280c8eeccbad7d
Joni <rautiojonathan@gmail.com>
2023-01-14 12:33:56 +0100

    updated func and style

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
 let printMode = false;
-const btn = document.getElementById("generate-btn");
+const generateBtn = document.getElementById("generate-btn");
+const printBtn = document.getElementById("print-btn");
 
-function togglePrintMode() {
+function togglePrintMode()
+{
   printMode = !printMode;
-  btn.textContent = printMode ? "Skriv ut" : "Klicka och generera";
+  generateBtn.textContent = printMode ? "Rensa" : "Klicka för att generera";
+  printBtn.style.display = printMode ? "inline-block" : "none";
 }
 
-function generateUI() {
+function generateUI()
+{
   let html = "";
   let lowerLetters = [];
 
-  for (var i = 97; i <= 122; i++) {
+  for (var i = 97; i <= 122; i++)
+  {
     lowerLetters.push(String.fromCharCode(i));
-    if (String.fromCharCode(i) === "z") {
+    if (String.fromCharCode(i) === "z")
+    {
       lowerLetters.push("å", "ä", "ö");
       break;
     }
   }
 
-  let upperLetters = lowerLetters.map((letter) => letter.toUpperCase());
+  let upperLetters = lowerLetters.map(letter => letter.toUpperCase());
   let numbers = Array.from({ length: 10 }, (_, i) => i);
   let combinedArray = [].concat(lowerLetters, upperLetters, numbers);
 
-  combinedArray.forEach(function (element) {
+  combinedArray.forEach(function (element)
+  {
     html += "<h2>" + element + "</h2>";
   });
 
@@ -30,12 +37,23 @@ function generateUI() {
   togglePrintMode();
 }
 
-function printUI() {
-  if (printMode) {
-    window.print();
-  } else {
+function printUI(event)
+{
+  if (printMode)
+  {
+    if (event.target.id === "generate-btn" && generateBtn.textContent === "Rensa")
+    {
+      document.getElementById("output").innerHTML = "";
+      togglePrintMode();
+    } else
+    {
+      window.print();
+    }
+  } else
+  {
     generateUI();
   }
 }
 
-btn.addEventListener("click", printUI);
+generateBtn.addEventListener("click", printUI);
+printBtn.addEventListener("click", printUI);
